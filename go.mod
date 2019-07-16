50b549a60564840a010df79afde424e728d75e1f
kinsprite <qxqs11@126.com>
2019-07-16 15:58:03 +0800

    add APM

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.2
+	go.elastic.co/apm/module/apmgrpc v1.4.0
 	google.golang.org/grpc v1.22.0
 )
 
--- product.go
+++ product.go
@@ -28,6 +28,7 @@ import (
 
 	pb "github.com/kinsprite/producttest/pb"
 
+	"go.elastic.co/apm/module/apmgrpc"
 	"google.golang.org/grpc"
 )
 
@@ -53,7 +54,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.UnaryInterceptor(apmgrpc.NewUnaryServerInterceptor()))
 	pb.RegisterGreeterServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
