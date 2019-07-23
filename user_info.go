cda3c3ab00fecffb4df62b37281920543286f1af
kinsprite <qxqs11@126.com>
2019-07-24 00:42:43 +0800

    update UserInfo model

--- user_info.go
+++ user_info.go
@@ -2,6 +2,7 @@ package main
 
 // UserInfo  from user test service
 type UserInfo struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
